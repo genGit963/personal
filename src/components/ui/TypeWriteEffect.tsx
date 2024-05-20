c14a2e6e9c5b1db716134f5ed9fe0f02ea9b366d
Mahesh Bogati <117003503+genGit963@users.noreply.github.com>
2024-05-20 11:55:07 +0545

    hero upt 2

--- src/components/ui/TypeWriteEffect.tsx
+++ src/components/ui/TypeWriteEffect.tsx
@@ -4,18 +4,20 @@ export function TypewriterEffectSmoothTgpy() {
   const words = [
     {
       text: "I am Software Engineer",
+      className: "sm:text-2xl",
     },
     {
       text: "Mahesh Bogati",
-      className: "text-blue-300 dark:text-blue-500 text-6xl",
+      className:
+        "bg-gradient-to-r from-indigo-400 via-purple-200 to-black-100 text-transparent bg-clip-text text-4xl sm:text-6xl",
     },
   ];
   return (
     <div className="flex flex-col items-center justify-center h-[40rem] py-3 ">
       <TypewriterEffectSmooth words={words} />
       <p className="text-white-100 max-w-xl font-medium w-full mt-6 text-center">
-        I will be in web, database, network, programming, algo and
-        devices. <br /> On the way into the depth of Tech....
+        I will be in web, database, network, programming, algo and devices.{" "}
+        <br /> On the way into the depth of Tech....
       </p>
     </div>
   );
